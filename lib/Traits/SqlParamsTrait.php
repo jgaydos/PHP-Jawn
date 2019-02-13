6d074d7e4abfdea7e2e87eee54b6debb7564c139
jgaydos <15269036+jgaydos@users.noreply.github.com>
2019-02-13 07:59:52 -0500

    Updated to include named replacements

--- lib/Traits/SqlParamsTrait.php
+++ lib/Traits/SqlParamsTrait.php
@@ -6,15 +6,25 @@ trait SqlParamsTrait
 {
     /**
      * Replaces ? in SQL query with respective value from params array
+     * as well as named replcements (:one). All replacements are litteral.
      * @access  private
      * @param   string  $query    SQL query
      * @param   array  $params    SQL query params
      */
     private function params(string $query, array $params): string
 	{
-		foreach ($params as $param) {
-			if ($pos = strpos($query, '?') !== false) {
-                $query = substr_replace($query, $param, $pos, strlen('?'));
+		if (array_keys($params) !== range(0, count($params) - 1)) {
+            // associative array
+            foreach ($params as $name => $param) {
+                $name = ':'.ltrim($name, ':');
+                $query = str_replace($name, $param, $query);
+            }
+        } else {
+            // sequencial array
+            foreach ($params as $param) {
+                if ($pos = strpos($query, '?') !== false) {
+                    $query = substr_replace($query, $param, $pos, strlen('?'));
+                }
             }
         }
 		return $query;
