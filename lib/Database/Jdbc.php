53f477e0ad1c41c0c3a1455d25fdca9e49e46a37
jgaydos <15269036+jgaydos@users.noreply.github.com>
2019-02-15 15:48:37 -0500

    Jdbc class

---
+++ lib/Database/Jdbc.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Jawn\Database;
+
+class Jdbc
+{
+    use \Traits\SqlParamsTrait;
+
+    private $_conn;
+
+    /**
+     * Constructor creates connection
+     *
+     * @access  public
+     * @param   object  $connection
+     */
+    public function __construct(object $connection)
+    {
+        $host = $connection->host ?? 'localhost';
+        $port = $connection->port ?? 1433;
+        $database = $connection->database ?? 'master';
+        $username = $connection->username ?? '';
+        $password = $connection->password ?? '';
+        $agentHost = $connection->agent->host ?? 'localhost';
+        $agentPort = $connection->agent->port ?? '4444';
+
+        if ($connection->type === 'openedge') {
+            $connStr = "JDBC:datadirect:openedge://$host:$port;DatabaseName=$database;";
+        } elseif ($connection->type === 'sqlserver') {
+            $connStr = "jdbc:sqlserver://$host:$port;databasename=$database;";
+        } elseif ($connection->type === 'oracle') {
+            $connStr = "jdbc:oracle:thin:$username/$password@$host:$port:$database";
+        } else {
+            Console::danger('JDBC driver not configured!');
+        }
+
+        $this->_conn = new \PJBridge($agentHost, $agentPort);
+        $result = $this->_conn->connect($connStr, $username, $password);
+        if (!$result) {
+            \Console::danger("Failed to connect\n");
+        }
+
+    }
+
+    /**
+     * This runs a query...
+     *
+     * @access  public
+     * @param   string  $sql    SQL query
+     * @param   array   $params Query parameters
+     * @return  array   $ofTheKing    Query results
+     */
+    public function query(string $sql, array $params = []): array
+    {
+        $sql = $this->params($sql, $params);
+        $cursor = $this->_conn->exec($sql);
+        $ofTheKing = [];
+        while ($row = $this->_conn->fetch_array($cursor)) {
+            $ofTheKing[] = $row;
+        }
+        return $ofTheKing;
+        $this->_conn->free_result($cursor);
+    }
+
+    /**
+     * This executes a query...
+     *
+     * @access  public
+     * @param   string  $sql    SQL query
+     * @param   array   $params Query parameters
+     * @return  void
+     */
+    public function execute(string $sql, array $params = []): void
+    {
+        $this->query($sql, $params);
+    }
+}
