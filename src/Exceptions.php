32e9f962c92781975f3c85d349d099c6adc16397
jgaydos <15269036+jgaydos@users.noreply.github.com>
2019-02-18 19:24:18 -0500

    Added dumpster exception

--- src/Exceptions.php
+++ src/Exceptions.php
@@ -5,6 +5,9 @@ class CofferEmptyException extends Exception {};
 class CofferHandleException extends Exception {};
 class CofferQueryException extends Exception {};
 
+// Dumpster
+class DumpsterHandleException extends Exception {};
+
 // Database
 class DatabaseConnectionException extends Exception {};
 class DatabaseQueryException extends Exception {};
