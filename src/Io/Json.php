7a463961c84cc59771fbd8cd33504ca9f62bdd88
Jon Gaydos <15269036+jgaydos@users.noreply.github.com>
2019-05-08 14:47:24 -0400

    Create Json.php

---
+++ src/Io/Json.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jawn\Io;
+
+/**
+ * You put it in and take it out
+ */
+class Json
+{
+    /**
+     * Extract or read from source and store as an array
+     *
+     * @access  public
+     * @param   string  $path   Source
+     * @param   string  $options   Extract options
+     * @return  array
+     */
+    public static function extract(string $path, array $options = []): array
+    {
+        $ofTheKing = [];
+        
+        return $ofTheKing;
+    }
+
+    /**
+     * Load or write array to destination
+     *
+     * @access  public
+     * @param   string  $path   Destination
+     * @param   array   $data   Data to save
+     * @param   string  $options   Loas options
+     * @return  array
+     */
+    public static function load(string $path, array $data, array $options = []): void
+    {
+        
+    }
+}
