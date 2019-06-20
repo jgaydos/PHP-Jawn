f293bf4309337fa9af7189caf7e1d33260d0978c
jgaydos <15269036+jgaydos@users.noreply.github.com>
2019-06-20 11:36:35 -0400

    Detect type

--- src/Io.php
+++ src/Io.php
@@ -10,11 +10,20 @@ class Io
      * @param   array   $options
      * @return  array
      */
-    public static function extract(
-        string $type,
-        string $source,
-        array $options = []
-    ): array {
+    public static function extract(): array
+    {
+        $args = func_get_args();
+
+        if (is_string($args[1] ?? [])) {
+            $type = $args[0];
+            $source = $args[1];
+            $options = $args[2] ?? [];
+        } else {
+            $source = $args[0];
+            $type = strtolower(pathinfo($source, PATHINFO_EXTENSION));
+            $options = $args[1] ?? [];
+        }
+
         $class = 'Jawn\Io\\'.ucfirst($type);
         if (!class_exists($class)) {
             throw new \IoNotImplementedException("$type does not exist.");
@@ -29,12 +38,23 @@ class Io
      * @param   array   $options
      * @return  void
      */
-    public static function load(
-        string $type,
-        string $target,
-        array $data,
-        array $options = []
-    ): void {
+    public static function load(): void
+    {
+        $args = func_get_args();
+
+        if (is_string($args[1])) {
+            $type = $args[0];
+            $target = $args[1];
+            $data = $args[2];
+            $options = $args[3] ?? [];
+        } else {
+            $target = $args[0];
+            touch($target);
+            $type = strtolower(pathinfo($target, PATHINFO_EXTENSION));
+            $data = $args[1];
+            $options = $args[2] ?? [];
+        }
+
         $class = 'Jawn\Io\\'.ucfirst($type);
         if (!class_exists($class)) {
             throw new \IoNotImplementedException("$type does not exist.");
